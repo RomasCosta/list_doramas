e6700a4cbf4ceb0362ac5767b3bd9b9f81c7b47e
RomasCosta <romariocosta25@outlook.com>
2023-07-11 14:53:48 -0300

    Project started

---
+++ src/main/java/com/dramalist/dmlist/DmlistApplication.java
@@ -0,0 +1,13 @@
+package com.dramalist.dmlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dramalist/dmlist/DmlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dramalist.dmlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
