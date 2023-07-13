fa30152aed5aaa89ff4772b6b0dffe484a581cb3
RomasCosta <romariocosta25@outlook.com>
2023-07-13 12:58:21 -0300

    CORS config

---
+++ src/main/java/com/dramalist/dmlist/config/WebConfig.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.dramalist.dmlist.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ *
+ * @author romar
+ */
+@Configuration
+public class WebConfig {//libera backend pra url front
+
+    @Value("${cors.origins}")//vem do application.properties
+    private String corsOrigins;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("*").allowedOrigins(corsOrigins);
+            }
+        };
+    }
+}
